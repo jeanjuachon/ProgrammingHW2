d773963775115b179b88731cdee9c02406f6b3a3
jeanphilip <jeanphilipjuachon28@gmail.com>
2019-05-05 13:42:56 +0800

    for programming assessment2

---
+++ script01.R
@@ -0,0 +1,36 @@
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  
+  
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+ 
+  getinv <- function() inv
+  setinv <- function(inverse) inv <<- inverse
+  
+  
+  list(get=get, set=set, getinv=getinv, setinv=setinv)
+}
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  
+  if (!is.null(inv)) {
+    message("Cached inverse")
+    return(inv)
+  }
+  
+  m <- x$get()
+  inv <- solve(m, ...)
+  
+
+  x$setinv(inv)
+  
+
+  return(inv)
+}

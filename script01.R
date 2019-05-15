5ba95e8e01d45d78f519ee908b645f03e5b2c750
jeanphilip <jeanphilipjuachon28@gmail.com>
2019-05-15 23:11:32 +0800

    added the comments and instructions

--- script01.R
+++ script01.R
@@ -1,32 +1,33 @@
 makeCacheMatrix <- function(x = matrix()) {
-  
+  #inverse of matrix that would be cached
   inv <- NULL
   
-  
+  #get and set of matrix
   get <- function() x
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   
- 
+ #get and set of matrix inverse
   getinv <- function() inv
   setinv <- function(inverse) inv <<- inverse
   
-  
+  #list
   list(get=get, set=set, getinv=getinv, setinv=setinv)
 }
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinv()
   
+  #computed matrix inverse
   if (!is.null(inv)) {
     message("Cached inverse")
     return(inv)
   }
-  
+  #compute
   m <- x$get()
-  inv <- solve(m, ...)
+  inv <- solve(m)
   
 
   x$setinv(inv)
